0a571c1b16d737238c5e5fb8c7a8a45ca881c775
Naresh Kumar <121565671+nareshkumar29@users.noreply.github.com>
2023-11-10 17:43:19 +0530

    Add files via upload

---
+++ Project - 07 Happy Diwali/script.js
@@ -0,0 +1,9 @@
+function happyDiwali(){
+    const diwali = document.getElementById("diwali");
+    const button = document.getElementById("clickme");
+
+
+    button.style.display = 'none';
+
+    diwali.style.display = 'block';
+}
